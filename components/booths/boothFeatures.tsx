b65266101376ffc8d9e44a65058f7b9bf17d3533
Anthony contreras <amazecpk@gmail.com>
2023-07-02 16:37:23 -0700

    continued boothFeatures styling updates

--- components/booths/boothFeatures.tsx
+++ components/booths/boothFeatures.tsx
@@ -24,25 +24,52 @@ const BoothFeatures = (props: PropsType) => {
 					:
 					<p>The Following Features Are Included In Preimum And Magic Mirror Photo Booth Packages</p>
 				}
-				<div className='flex flex-wrap mx-20 space-x-10 justify-center'>
-
+				<div className='flex flex-wrap   justify-center'>
 					{includedFeatures.map((el, index) => (
-						<div key={index} className='w-1/4 text-center flex flex-col items-center space-y-4 mt-10'>
+						<div key={index} className='w-1/4 mx-4 text-center flex flex-col items-center space-y-4 mt-10'>
 							<img src={el.imagePath} alt="" className='h-[233px] w-[350px] object-cover rounded-3xl' />
-							<p className=''><span className='text-primary'>•</span> {el.description} <span className='text-primary'>•</span></p>
+							<div className='flex justfify-center'>
+								<span className='text-primary'>•</span>
+								<p>&nbsp;{el.description}&nbsp;</p>
+								<span className='text-primary'>•</span>
+							</div>
 						</div>
 					))}
 				</div>
 			</div>
 
 			<div className='flex flex-col items-center mt-20'>
+				<p className={`${scriptFont.className} text-6xl mb-5`}>Optional Features</p>
+				{props.optionalFeaturesDescription ?
+					<p>{props.optionalFeaturesDescription}</p>
+					:
+					<p>The Following Optional Features Are For Preimum And Magic Mirror Photo Booth Packages</p>
+				}
+				<div className='flex flex-wrap   justify-center'>
+					{OptionalFeatures.map((el, index) => (
+						<div key={index} className='w-1/4 mx-4 text-center flex flex-col items-center space-y-4 mt-10'>
+							<img src={el.imagePath} alt="" className='h-[233px] w-[350px] object-cover rounded-3xl' />
+							<div className='flex justfify-center'>
+								<span className='text-primary'>•</span>
+								<p>&nbsp;{el.description}&nbsp;</p>
+								<span className='text-primary'>•</span>
+							</div>
+							{/* <p className=''><span className='text-primary'>•</span> {el.description} <span className='text-primary'>•</span></p> */}
+						</div>
+					))}
+				</div>
+			</div>
+
+			
+
+			{/* <div className='flex flex-col items-center mt-20'>
 				<p className={`${scriptFont.className} text-6xl mb-5 text-center`}>Additional Optional Features</p>
 				{props.optionalFeaturesDescription ?
 					<p>{props.optionalFeaturesDescription}</p>
 					:
 					<p>The Following Optional Features Are For Preimum And Magic Mirror Photo Booth Packages</p>
 				}
-				<div className='flex flex-wrap mx-20 space-x-10 justify-center'>
+				<div className='flex flex-wrap  space-x-10 justify-center'>
 
 					{OptionalFeatures.map((el, index) => (
 						<div key={index} className='w-1/4 text-center flex flex-col items-center space-y-4 mt-10'>
@@ -51,7 +78,7 @@ const BoothFeatures = (props: PropsType) => {
 						</div>
 					))}
 				</div>
-			</div>
+			</div> */}
 		</section>
 	)
 }
