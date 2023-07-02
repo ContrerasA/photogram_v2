6c37af0bb0ea3cae2f66e85764f3c149dd171550
Anthony contreras <amazecpk@gmail.com>
2023-07-02 16:40:02 -0700

    Gallery mobile compatibility

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -11,10 +11,10 @@ const Gallery = () => {
 			<p>Here are a few shots of our guests having a blast in in our Photogram Photo Booths</p>
 			<div className='flex justify-center'>
 
-			<div className='flex flex-wrap m-20 w-3/4'>
+			<div className='flex flex-wrap lg:m-20 lg:w-3/4 justify-center'>
 				{
 					galleryImages.map(el => (
-						<div key={el.id} className='basis-1/4 p-10'>
+						<div key={el.id} className='lg:p-10 p-2'>
 							<img src={el.path} alt={el.ariaText} className='object-cover rounded-3xl h-[275px] w-[275px]'/>
 						</div>
 					))
