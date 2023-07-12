b452a5c7944be3abf81951d64a6af9bb4402220e
Anthony contreras <amazecpk@gmail.com>
2023-07-12 15:48:01 -0700

    Added conversion tag for google ads

--- app/book-now/[id]/thank-you/page.tsx
+++ app/book-now/[id]/thank-you/page.tsx
@@ -1,18 +1,31 @@
 import Confetti from '@/components/global/Confetti'
 import { scriptFont } from '@/src/utils/fontUtils'
+import Script from 'next/script'
 import React from 'react'
 
 const BookingThankYou = () => {
-  return (
-	  <section className='text-center h-[500px] flex flex-col justify-center relative'>
-		  <p className={`${scriptFont.className} text-6xl`}>Contratulations!</p>
-		  <p>You&#39;re all set!</p>
-		  <p>Thank You For Booking With Photogram Photo Booth</p>
-		  <p>We&#39;ll contact you shortly to confirm, as well as how to proceed with the next steps for your booking</p>
-			
-		  <Confetti/>
-	 </section>
-  )
+	return (
+		<>
+			<Script>
+				{`
+				  gtag('event', 'conversion', {
+						'send_to': 'AW-10801341731/hc16COaQ47oYEKPKvZ4o',
+						'value': 35.0,
+						'currency': 'USD',
+						'transaction_id': ''
+				  });
+				`}
+			</Script>
+			<section className='text-center h-[500px] flex flex-col justify-center relative'>
+				<p className={`${scriptFont.className} text-6xl`}>Contratulations!</p>
+				<p>You&#39;re all set!</p>
+				<p>Thank You For Booking With Photogram Photo Booth</p>
+				<p>We&#39;ll contact you shortly to confirm, as well as how to proceed with the next steps for your booking</p>
+
+				<Confetti />
+			</section>
+		</>
+	)
 }
 
 export default BookingThankYou
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import Footer from '@/components/global/Footer'
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
 import { useEffect } from 'react'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,36 +22,31 @@ export default function RootLayout({
 	children: React.ReactNode
 }) {
 
-	// @ts-ignore
-	function tagManager(w, d, s, l, i) {
-		w[l] = w[l] || []; w[l].push({
-			'gtm.start':
-				new Date().getTime(), event: 'gtm.js'
-		}); var f = d.getElementsByTagName(s)[0],
-			j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-			'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-		console.log('test')
-		console.log('test')
-	}
-
-	// tagManager(window, document, 'script', 'dataLayer', 'GTM-W8J2W3Z')
-	// useEffect(() => {
-		
-	// })
 
 	return (
 		<html lang="en" data-theme='light'>
-			<Head>
-				<script id='gtm-script'
-				dangerouslySetInnerHTML={{__html:`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+
+			<Script id='google-analytics'>
+				{`
+				(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 				new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 				j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 				'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-				})(window,document,'script','dataLayer','GTM-W8J2W3Z');`}}
-				>
+				})(window,document,'script','dataLayer','GTM-W8J2W3Z')
+				`}
+			</Script>
 
-				</script>
-			</Head>
+			<Script async src='https://www.googletagmanager.com/gtag/js?id=AW-10801341731' />
+
+			<Script>
+				{`
+				window.dataLayer = window.dataLayer || [];
+				function gtag(){dataLayer.push(arguments);}
+				gtag('js', new Date());
+			 
+				gtag('config', 'AW-10801341731');
+				`}
+			</Script>
 
 			<body className={inter.className}>
 				<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W8J2W3Z"
